68246efe142456c845a562c50ced2d99811f1f33
walnuts1018 <r.juglans.1018@gmail.com>
2026-05-05 01:34:51 +0900

    fix: add kubeadm bootstrap and control plane v1beta2 types to scheme
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- test/e2e/provisioning/scheme.go
+++ test/e2e/provisioning/scheme.go
@@ -4,6 +4,8 @@ import (
 	infrastructurev1alpha1 "github.com/walnuts1018/cluster-api-provider-tart/api/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	kubeadmbootstrapv1 "sigs.k8s.io/cluster-api/api/bootstrap/kubeadm/v1beta2"
+	kubeadmcontrolplanev1 "sigs.k8s.io/cluster-api/api/controlplane/kubeadm/v1beta2"
 	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 )
 
@@ -13,6 +15,8 @@ func newScheme() *runtime.Scheme {
 	_ = clientgoscheme.AddToScheme(scheme)
 	_ = infrastructurev1alpha1.AddToScheme(scheme)
 	_ = clusterv1.AddToScheme(scheme)
+	_ = kubeadmbootstrapv1.AddToScheme(scheme)
+	_ = kubeadmcontrolplanev1.AddToScheme(scheme)
 
 	return scheme
 }

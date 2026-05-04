ff77a6e90d46496c33a49f69be9dc70829e849eb
walnuts1018 <r.juglans.1018@gmail.com>
2026-05-04 12:00:54 +0900

    refactor: remove unused trace sampler tests from tracer_test.go
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- pkg/telemetry/tracer_test.go
+++ pkg/telemetry/tracer_test.go
@@ -2,7 +2,6 @@ package telemetry
 
 import (
 	"context"
-	"strings"
 	"testing"
 )
 
@@ -33,35 +32,5 @@ func TestNewTelemetryResourceUsesStandardSDKName(t *testing.T) {
 	}
 	if attrs["telemetry.sdk.name"] != "opentelemetry" {
 		t.Fatalf("telemetry.sdk.name = %q, want opentelemetry", attrs["telemetry.sdk.name"])
-	}
-}
-
-func TestDefaultTraceSamplerIsParentBasedAndConfigurableByEnv(t *testing.T) {
-	t.Setenv("OTEL_TRACES_SAMPLER", "parentbased_traceidratio")
-	t.Setenv("OTEL_TRACES_SAMPLER_ARG", "0.25")
-
-	sampler := defaultTraceSampler()
-	description := sampler.Description()
-
-	if !strings.Contains(description, "ParentBased") {
-		t.Fatalf("sampler description = %q, want ParentBased sampler", description)
-	}
-	if !strings.Contains(description, "TraceIDRatioBased{0.25}") {
-		t.Fatalf("sampler description = %q, want OTEL_TRACES_SAMPLER_ARG to be used", description)
-	}
-}
-
-func TestDefaultTraceSamplerFallsBackToTraceIDRatio(t *testing.T) {
-	t.Setenv("OTEL_TRACES_SAMPLER", "")
-	t.Setenv("OTEL_TRACES_SAMPLER_ARG", "")
-
-	sampler := defaultTraceSampler()
-	description := sampler.Description()
-
-	if !strings.Contains(description, "ParentBased") {
-		t.Fatalf("sampler description = %q, want ParentBased sampler", description)
-	}
-	if !strings.Contains(description, "TraceIDRatioBased{0.1}") {
-		t.Fatalf("sampler description = %q, want default trace sample rate", description)
 	}
 }

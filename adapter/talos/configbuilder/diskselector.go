abe68d1276f2b3c2304c2de823a743bb69bf25e1
walnuts1018 <r.juglans.1018@gmail.com>
2026-09-07 22:17:02 +0900

    fix: TalosのCEL依存パスを統一
    Signed-off-by: walnuts1018 <r.juglans.1018@gmail.com>

--- adapter/talos/configbuilder/diskselector.go
+++ adapter/talos/configbuilder/diskselector.go
@@ -1,7 +1,7 @@
 package configbuilder
 
 import (
-	googlecel "cel.dev/cel-go/cel"
+	googlecel "github.com/google/cel-go/cel"
 	"github.com/siderolabs/talos/pkg/machinery/cel"
 
 	domainbootstrap "github.com/walnuts1018/cluster-api-provider-tart/domain/bootstrap"
